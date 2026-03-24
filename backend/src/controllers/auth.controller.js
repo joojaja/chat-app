1a10953bf1b3c89eb29056c5cc568abc5803c26c
joojaja <194677730+joojaja@users.noreply.github.com>
2026-03-24 19:39:02 +0800

    complete login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,8 +51,30 @@ export const signup = async (req, res) => { // use async so can use await
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email}); // find in database
+        if (!user) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password); // check if password is correct (compare with hashed password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        generateToken(user._id, res) // generate JWT token
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
 
 export const logout = (req, res) => {
