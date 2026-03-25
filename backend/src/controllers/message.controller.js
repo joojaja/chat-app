22ffb70a08c6027ed780db0a191709d7c760f843
joojaja <194677730+joojaja@users.noreply.github.com>
2026-03-25 11:27:39 +0800

    add getMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,14 +1,32 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async(req, res) => {
     // get every user except myself
-
     try {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password"); // select all != my userId and dont select password
         res.status(200).json(filteredUsers)
     } catch (error) {
         console.log("Error in getUsersForSidebar: ", error.messsage);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const getMessages = async(req, res) => {
+    try {
+        const {id:userToChatId} = req.params; // get parameter id and rename to userToChatId
+        const myId = req.user._id;
+
+        const messages = await Message.find({ // find all messages where sender is me and receiver is other user or other way around
+            $or:[
+                {senderId:myId, receiverId: userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        })
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages: ", error.messsage);
         res.status(500).json({message: "Internal server error"});
     }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-  
+router.get("/:id", protectRoute, getMessages); //userId that we want to fetch the messages  
 
 export default router;
