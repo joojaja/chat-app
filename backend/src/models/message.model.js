094a975266648697475d1be3ab6e942b6d845f2c
joojaja <194677730+joojaja@users.noreply.github.com>
2026-03-25 11:11:23 +0800

    complete message model

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+ {
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true, 
+    },
+
+    text: {
+        type: String,
+    },
+
+    image: {
+        type: String,
+    },
+ }, 
+ { timestamps: true}
+);
+
+const Message = mongoose.model("Message", messageSchema); // name of collection + schema. user upper case first letter + singular form
+// mongoose will auto save collection name as "messages" in database
+
+export default Message;
