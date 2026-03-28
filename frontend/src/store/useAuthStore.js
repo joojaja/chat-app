43f3383a51734bd8639d84f7225d460c5da3bc4c
joojaja <194677730+joojaja@users.noreply.github.com>
2026-03-28 15:34:22 +0800

    finish signup function in zustand

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -21,7 +21,18 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    signup: async ({fullName, email, password}) => {
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
 
-    }
+        } catch (error) {
+            toast.error(error.response.data.message);
+
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
 }))
