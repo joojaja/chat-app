9425450ff439d6e0c48876449666a582a8be9488
joojaja <194677730+joojaja@users.noreply.github.com>
2026-04-02 22:11:30 +0800

    initialise socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // allows to extract json data from request body
@@ -21,7 +22,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
     connectDB(); // calls connectDB function in ./lib/db.js
 })
