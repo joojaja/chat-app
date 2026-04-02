47d33032e3debcb7cbfe1a6d337977da6bddfdce
joojaja <194677730+joojaja@users.noreply.github.com>
2026-04-02 22:29:15 +0800

    add comments

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -19,18 +19,21 @@ export function getReceiverSocketId(userId) {
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-  console.log("A user has connected: ", socket.id);
+  console.log("A user has connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
   // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap)); // "getOnlineUsers" is the event name, Object.keys(userSocketMap) is the data we are sending to clients, which is an array of online userIds
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected: ", socket.id);
+    console.log("A user has disconnected:", socket.id);
     delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // send events to all the connected clients
   });
 });
 
