f91960f051c00c4d42ed531c1b2383f171221404
joojaja <194677730+joojaja@users.noreply.github.com>
2026-04-03 12:13:47 +0800

    fix production endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,14 @@ import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path"; // in-built import to work with file and directory paths
 
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); // allows to extract json data from request body
 app.use(cookieParser()); // allows parsing of cookie
@@ -21,6 +23,15 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// if using production environment, serve the frontend from the backend server
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
